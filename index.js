b3052c6899f5d9f8d215794c194c8ce83b7d5657
Eoloo <evans.oloo@student.moringaschool.com>
2022-10-19 17:11:20 +0300

    files updated

---
+++ index.js
@@ -0,0 +1,96 @@
+
+//document.getElementById("count-el").innerText = 4
+// TO DO
+// intialize the count as 0
+//let count = 0
+// listen for clicks on the increment button
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+//change the count-el in the HTML to reflect the new count
+//let countEl = document.getElementById("count-el")
+//console.log(countEl)
+//incrementLap()
+
+//console.log(lapsCompleted)
+//CREATING A FUNCTION
+//function countdown() {
+    //console.log(5)
+    //console.log(4)
+    //console.log(3)
+    //console.log(2)
+    //console.log(1)
+//}
+//countdown
+//example
+//function entries(){
+    //console.log(42)
+//}
+//--entries()
+//EXAMPLE 2
+//let lap1 = 34
+//let lap2 = 33
+//let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+//function logLapTime() {
+    //let totalTime = lap1 + lap2 + lap3
+    //console.log(totalTime)
+//}
+//logLapTime()
+
+//EXAMPLE 3
+//let lapsCompleted = 0
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+//function incrementLap() {
+    //lapsCompleted = lapsCompleted + 1
+//}
+
+//incrementLap()
+//incrementLap()
+//incrementLap()
+
+//console.log(lapsCompleted)
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+// creating a variable
+// let firstBatch = 4
+
+// let secondBatch = 10
+
+// let count = firstBatch + secondBatch
+
+// count = count + 2
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// let myAge = 33
+
+// let humanDogRatio = 7
+
+// myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// bonusPoints = bonusPoints - 75
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+
